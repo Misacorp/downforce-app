226c490625e37fede20099e544a6c13a4e79428a
Misa Jokisalo <misa.jokisalo@knowit.fi>
2022-09-01 21:29:44 +0300

    Disable refetching on window focus

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Main from "@/pages/Main";
 import "@/globalStyles.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 /**
  * Application root. All context providers should be listed here.
